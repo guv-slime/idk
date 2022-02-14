5a2e19956448de6afd199c4413c54e509f587e95
guv-slime <dufusqrl@gmail.com>
2022-02-14 14:25:30 -0500

    made a favicon, homepage, can interact with the button. some JS gains

---
+++ main.js
@@ -0,0 +1,11 @@
+//starting count
+//on button click
+//call ++ method
+//return count
+
+let myButton = document.getElementById('idkButton');
+myButton.addEventListener('click', idkPlus);
+
+function idkPlus(){
+    console.log('I LISTEN TO THE BUTTON');
+}
